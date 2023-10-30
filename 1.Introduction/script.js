d3efd10eb724386d3a3660549abaf46ac41da694
Kilian Boute <kilian.boute@gmail.com>
2023-10-30 11:51:04 +0100

    complete exercise 1 + 2

--- 1.Introduction/script.js
+++ 1.Introduction/script.js
@@ -1,2 +1,29 @@
-/*
+/*Display the document title in the console
+Change the document title to Modifying the DOM
+Change the background color of the body to hot pink (#FF69B4). 
+If that worked, try with a random color as an extra challenge. 
+(tip: You can use the rgb() notation and 3 random-generated 
+numbers between 0 and 255).
+Using the children method and a for ... of  loop, display every children elements 
+of the second child element of your document (display all children elements of the 
+    <body>)
 */
+
+console.log(document.title);
+
+document.title = "Modifying the DOM";
+
+let body = document.querySelector("body");
+
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+body.style.backgroundColor = "rgb(" + getRandomInt(255) 
+    + ", " + getRandomInt(255) + ", " + getRandomInt(255) + ")";
+
+for (const child of body.children) {
+    console.log(child);
+}
--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,2 +1,61 @@
 /*
+Add a title attribute to every element that has the important class, stating This 
+is an important item. Tip: adding a title attribute to an element is different 
+from changing the title of a document.
 */
+
+let important = document.querySelectorAll(".important");
+
+important.forEach(element => {
+    element.title = "This is an important item";
+    console.log(element);
+});
+
+/*- Select all the img tags and loop through them. 
+If they have no `important` class, turn their 
+[display property](https://developer.mozilla.org/en-US/docs/Web/CSS/display) 
+to `none`*/
+
+let images = document.querySelectorAll("img");
+
+images.forEach(element => {
+    if(element.className === "important"){
+        element.style.display = "none";
+    }
+    console.log(element.alt);
+});
+
+/*
+Loop through all the paragraphs and display their content in the console. 
+If the paragraph has a class, display its classname as well
+*/
+
+let paragraphs = document.querySelectorAll("p");
+
+paragraphs.forEach(element => {
+    console.log(element.innerHTML);
+    if(element.hasAttribute("class")){
+        console.log(element.className);
+    }
+});
+
+/*
+Give each of the paragraph a random text color (different for each one) 
+UNLESS it has a class then leave it as it is.
+*/
+
+function getRandomColor() {
+    let a = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let c = Math.floor(Math.random() * 255);
+
+    let color =  "rgb(" + a + ", " + b + ", " + c + ")";
+    console.log(color);
+    return color;
+}
+
+paragraphs.forEach(element => {
+    if(!element.hasAttribute("class")){
+        element.style.color = getRandomColor();
+    }
+});
